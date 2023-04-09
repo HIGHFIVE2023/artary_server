0ce13e8e3410900a6bf047d6bdb566e687205b6b
soyeonvv <sovvworld@gmail.com>
2023-04-10 00:47:33 +0900

    프로젝트 생성

---
+++ src/main/java/com/highfive/artary/ArtaryApplication.java
@@ -0,0 +1,13 @@
+package com.highfive.artary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/highfive/artary/ArtaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.highfive.artary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
